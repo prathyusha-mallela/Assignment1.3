61eb1a2bfc5cb2b2b8399d8684cd25f1c9026b20
prathyusha-mallela <pratyushamv@gmail.com>
2017-12-23 17:52:54 +0530

    Add files via upload

---
+++ Assignment13.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 23 17:42:31 2017
+
+@author: Prathyusha Mallela
+"""
+#Assignment 1.3
+def get_last_first_name(first_name,last_name):
+    full_name=last_name+' '+first_name
+    return full_name
+
+#example
+full_name=get_last_first_name('Bruce','Wayne')
+print("The fule name in the form lastname, firstname would be : "+full_name);
